2d7a69302f81cb7143d2370c69523ee551120078
John <iplanetcn@gmail.com>
2017-09-05 17:58:27 +0800

    initial commit

---
+++ app/src/main/java/com/phenix/quality/CheckStyleExample.java
@@ -0,0 +1,24 @@
+package com.phenix.quality;
+
+
+import android.util.Log;
+
+/**
+ * CheckStyleExample
+ *
+ * @author john
+ * @since 2017-09-05
+ */
+@SuppressWarnings("ALL")
+public class CheckStyleExample {
+    public static final int xXx_$x$$$ = 0;
+        private Integer PP = new Integer(xXx_$x$$$);
+    private
+    void
+    printInfo
+            (String
+                     message)
+    {System.out.println(message);Log.d(CheckStyleExample.class.getSimpleName(),message);}
+
+
+}
